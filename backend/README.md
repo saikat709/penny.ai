01691555e463c8a92d6645ee184e9bdb02902ffe
Mohammad Saikat Islam <saikatislam709@gmail.com>
2025-08-14 21:44:58 +0600

    backend project setup

---
+++ backend/README.md
@@ -0,0 +1,15 @@
+# # Penny.AI Backend 
+
+```bash
+./mvnw clean install
+```
+
+
+```bash
+# Compile and run in one step
+./mvnw spring-boot:run
+
+# Or compile first, then run the jar
+./mvnw clean package
+java -jar target/pennyai-0.0.1-SNAPSHOT.jar
+```
---
+++ backend/src/main/java/com/penny/PennyAIApplication.java
@@ -0,0 +1,16 @@
+package com.penny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class PennyAIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PennyAIApplication.class, args);
+
+	}
+
+}
