90a28fd67f8ed7f1852ccda420b30ab93360c307
Md. Saikat Islam <saikatislam709@gmail.com>
2025-09-09 11:57:07 +0600

    chrome ext.

---
+++ browser-extensions/chrome/src/popup/App.tsx
@@ -0,0 +1,10 @@
+import HelloWorld from '@/components/HelloWorld'
+import './App.css'
+
+export default function App() {
+  return (
+    <div>
+      <HelloWorld msg="Penny.AI" />
+    </div>
+  )
+}
